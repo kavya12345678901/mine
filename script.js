575f0343cf38c6dec078fd52f58b37e61269dc84
Nithya <shreeni034@gmail.com>
2025-05-19 12:06:11 -0400

    Initial commit - Simple To-Do App

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTask() {
+	const input = document.getElementById("taskInput");
+	const taskText = input.value.trim();
+	if (taskText == "") return;
+	const li = document.createElement("li");
+	li.textContent = taskText;
+	li.onclick = () => li.remove();
+	document.getElementById("taskList").appentChild(li);
+	input.value = "";
+}
